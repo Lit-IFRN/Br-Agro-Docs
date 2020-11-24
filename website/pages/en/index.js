732e42ddbacc46e32ab285ef8f8f32945f5dc0c6
Bruno Eduardo de Souza Medeiros <brunooeduardoop@gmail.com>
2020-11-24 18:16:13 -0300

    changed index.html to index.js

---
+++ website/pages/en/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Index = () => {
+
+  window.location.href="docs/en/collector-intro.html";
+
+  return(
+    <div>
+      If you are not redirected automatically, follow this
+      <a href="docs/en/collector-intro.html">link</a>.
+    </div>
+  )
+}
+
+export default Index;
---
+++ website/pages/pt-BR/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Index = () => {
+
+  window.location.href="docs/en/collector-intro.html";
+
+  return(
+    <div>
+      Se você não foi redirecionado automáticamente, clique nesse
+      <a href="docs/pt-BR/collector-intro.html">link</a>.
+    </div>
+  )
+}
+
+export default Index;
